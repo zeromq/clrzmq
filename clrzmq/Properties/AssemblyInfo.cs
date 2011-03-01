4b2ea9ae021e3e22ef7295d3e59fc661c9f865f4
Michael Compton <michael@michael-laptop.(none)>
2011-03-01 20:34:17 +0000

    Increment Assembly version to 2.1.0.0 to reflect targetted ZeroMQ version and added some info

--- clrzmq/Properties/AssemblyInfo.cs
+++ clrzmq/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("clrzmq")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("clrzmq2")]
+[assembly: AssemblyDescription("CLR interop for ZeroMQ")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("clrzmq")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("clrzmq2")]
+[assembly: AssemblyCopyright("Copyright © Michael Comptons")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.0")]
