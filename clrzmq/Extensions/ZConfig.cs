9be4c703067fbe86f4db6bdae391c2d490e9dbf5
hconceicao <henry.conceicao@gmail.com>
2011-08-24 01:15:11 -0300

    added some adapters/acessors to enabled unit testing

---
+++ clrzmq/Extensions/ZConfig.cs
@@ -0,0 +1,23 @@
+﻿namespace clrzmq.Extensions
+{
+	using ZMQ;
+
+	public class ZConfig
+	{
+		public ZConfig(string ip, uint port, SocketType socketType, Transport? transport = Transport.TCP)
+		{
+			SocketType = socketType;
+			Transport = transport.Value;
+			Ip = ip;
+			Port = port;
+		}
+
+		public SocketType SocketType { get; set; }
+
+		public Transport Transport { get; set; }
+
+		public string Ip { get; set; }
+
+		public uint Port { get; set; } 
+	}
+}
