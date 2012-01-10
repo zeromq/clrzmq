25ffb1b7a90fb6bf3817e70a1c02f44a2f8ae22a
John Gozde <johnny@jgoz.net>
2012-01-10 00:48:50 -0700

    Add SimpleTests project for smoke testing.

---
+++ src/ZeroMQ.SimpleTests/HelloWorld.cs
@@ -0,0 +1,57 @@
+﻿namespace ZeroMQ.SimpleTests
+{
+    using System;
+    using System.IO;
+    using System.Text;
+    using System.Threading;
+
+    internal class HelloWorld
+    {
+        public void RunTest()
+        {
+            var client = new Thread(ClientThread);
+            var server = new Thread(ServerThread);
+
+            server.Start();
+            client.Start();
+
+            server.Join();
+            client.Join();
+        }
+
+        private static void ClientThread()
+        {
+            Thread.Sleep(10);
+
+            using (var context = ZmqContext.Create())
+            using (var socket = context.CreateSocket(SocketType.REQ))
+            {
+                socket.Connect("tcp://localhost:8989");
+
+                socket.Send(new Frame(Encoding.UTF8.GetBytes("Hello")));
+
+                var buffer = new byte[100];
+                int size = socket.Receive(buffer);
+
+                using (var stream = new MemoryStream(buffer, 0, size))
+                {
+                    Console.WriteLine(Encoding.UTF8.GetString(stream.ToArray()));
+                }
+            }
+        }
+
+        private static void ServerThread()
+        {
+            using (var context = ZmqContext.Create())
+            using (var socket = context.CreateSocket(SocketType.REP))
+            {
+                socket.Bind("tcp://*:8989");
+
+                Frame request = socket.Receive();
+                Console.WriteLine(Encoding.UTF8.GetString(request));
+
+                socket.Send(new Frame(Encoding.UTF8.GetBytes("World")));
+            }
+        }
+    }
+}
---
+++ src/ZeroMQ.SimpleTests/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ZeroMQ.SimpleTests
+{
+    using System;
+
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Running test HelloWorld...");
+            Console.WriteLine();
+
+            var helloWorld = new HelloWorld();
+            helloWorld.RunTest();
+
+            Console.WriteLine();
+            Console.WriteLine("Press enter key to exit...");
+            Console.ReadLine();
+        }
+    }
+}
