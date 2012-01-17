4b75f0debc30a4fda04a2ced1e352b4a059632ad
John Gozde <johnny@jgoz.net>
2012-01-17 00:46:03 -0700

    Refactor ZmqVersion to be more C#-like.

--- src/ZeroMQ/ZmqVersion.cs
+++ src/ZeroMQ/ZmqVersion.cs
@@ -8,44 +8,46 @@
     /// <summary>
     /// Provides ZeroMQ version information.
     /// </summary>
-    public static class ZmqVersion
+    public class ZmqVersion
     {
         private static readonly int SizeofInt32 = Marshal.SizeOf(typeof(int));
 
-        /// <summary>
-        /// Write the current ZeroMQ version numbers to the provided values.
-        /// </summary>
-        /// <param name="major">The major version.</param>
-        /// <param name="minor">The minor version.</param>
-        /// <param name="patch">The patch.</param>
-        public static void Version(out int major, out int minor, out int patch)
+        private static readonly Lazy<ZmqVersion> CurrentVersion;
+
+        static ZmqVersion()
         {
-            IntPtr majorPointer = Marshal.AllocHGlobal(SizeofInt32);
-            IntPtr minorPointer = Marshal.AllocHGlobal(SizeofInt32);
-            IntPtr patchPointer = Marshal.AllocHGlobal(SizeofInt32);
+            CurrentVersion = new Lazy<ZmqVersion>(GetCurrentVersion);
+        }
 
-            LibZmq.zmq_version(majorPointer, minorPointer, patchPointer);
-
-            major = Marshal.ReadInt32(majorPointer);
-            minor = Marshal.ReadInt32(minorPointer);
-            patch = Marshal.ReadInt32(patchPointer);
-
-            Marshal.FreeHGlobal(majorPointer);
-            Marshal.FreeHGlobal(minorPointer);
-            Marshal.FreeHGlobal(patchPointer);
+        private ZmqVersion(int major, int minor, int patch)
+        {
+            Major = major;
+            Minor = minor;
+            Patch = patch;
         }
 
         /// <summary>
-        /// Formats the current ZeroMQ version as a string.
+        /// Gets a <see cref="ZmqVersion"/> value for the current library version.
         /// </summary>
-        /// <returns>A string containing the current ZeroMQ version, formatted as "major.minor.patch".</returns>
-        public static string Version()
+        public static ZmqVersion Current
         {
-            int major, minor, patch;
-            Version(out major, out minor, out patch);
-
-            return major + "." + minor + "." + patch;
+            get { return CurrentVersion.Value; }
         }
+
+        /// <summary>
+        /// Gets the major version part.
+        /// </summary>
+        public int Major { get; private set; }
+
+        /// <summary>
+        /// Gets the minor version part.
+        /// </summary>
+        public int Minor { get; private set; }
+
+        /// <summary>
+        /// Gets the patch version part.
+        /// </summary>
+        public int Patch { get; private set; }
 
         /// <summary>
         /// Determine whether the current version of ZeroMQ meets the specified minimum required version.
@@ -53,12 +55,9 @@
         /// <param name="requiredMajor">An <see cref="int"/> containing the minimum required major version.</param>
         /// <param name="requiredMinor">An <see cref="int"/> containing the minimum required minor version.</param>
         /// <returns>true if the current ZeroMQ version meets the minimum requirement; false otherwise.</returns>
-        public static bool IsAtLeast(int requiredMajor, int requiredMinor)
+        public bool IsAtLeast(int requiredMajor, int requiredMinor)
         {
-            int major, minor, patch;
-            Version(out major, out minor, out patch);
-
-            return major >= requiredMajor && minor >= requiredMinor;
+            return Major >= requiredMajor && Minor >= requiredMinor;
         }
 
         /// <summary>
@@ -67,15 +66,38 @@
         /// <param name="requiredMajor">An <see cref="int"/> containing the minimum required major version.</param>
         /// <param name="requiredMinor">An <see cref="int"/> containing the minimum required minor version.</param>
         /// <exception cref="ZmqVersionException">The ZeroMQ version does not meet the minimum requirements.</exception>
-        public static void Assert(int requiredMajor, int requiredMinor)
+        public void Assert(int requiredMajor, int requiredMinor)
         {
-            int major, minor, patch;
-            Version(out major, out minor, out patch);
-
-            if (major < requiredMajor || (major == requiredMajor && minor < requiredMinor))
+            if (!IsAtLeast(requiredMajor, requiredMinor))
             {
-                throw new ZmqVersionException(major, minor, requiredMajor, requiredMinor);
+                throw new ZmqVersionException(Major, Minor, requiredMajor, requiredMinor);
             }
+        }
+
+        /// <summary>
+        /// Returns a <see cref="String"/> that represents the current <see cref="ZmqVersion"/>.
+        /// </summary>
+        /// <returns>A string containing the current ZeroMQ version, formatted as "major.minor.patch".</returns>
+        public override string ToString()
+        {
+            return Major + "." + Minor + "." + Patch;
+        }
+
+        private static ZmqVersion GetCurrentVersion()
+        {
+            IntPtr majorPointer = Marshal.AllocHGlobal(SizeofInt32);
+            IntPtr minorPointer = Marshal.AllocHGlobal(SizeofInt32);
+            IntPtr patchPointer = Marshal.AllocHGlobal(SizeofInt32);
+
+            LibZmq.zmq_version(majorPointer, minorPointer, patchPointer);
+
+            var version = new ZmqVersion(Marshal.ReadInt32(majorPointer), Marshal.ReadInt32(minorPointer), Marshal.ReadInt32(patchPointer));
+
+            Marshal.FreeHGlobal(majorPointer);
+            Marshal.FreeHGlobal(minorPointer);
+            Marshal.FreeHGlobal(patchPointer);
+
+            return version;
         }
     }
 }
