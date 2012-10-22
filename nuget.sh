4980507fae33978209fc81c4783a52446cd29023
David Mitchell <dave@fallingcanbedeadly.com>
2012-10-21 17:12:00 -0700

    Fix incorrect case in NuGet filename.

--- nuget.sh
+++ nuget.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-NUGET_EXE=src/.nuget/nuget.exe
+NUGET_EXE=src/.nuget/NuGet.exe
 PACKAGE_DIR=src/packages
 
 find . -name 'packages.config' -exec mono --runtime=v4.0 $NUGET_EXE install '{}' -o $PACKAGE_DIR \;
