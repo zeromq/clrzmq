0a16a93130286130ecc456f96d21956d53df53da
John Gozde <john@gozde.ca>
2012-11-10 00:41:24 -0700

    Initial conversion to NUnitLite.

---
+++ src/ZeroMQ.AcceptanceTests/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ZeroMQ.AcceptanceTests
+{
+    using NUnitLite.Runner;
+
+    /// <summary>
+    /// Main entry point for the acceptance test harness.
+    /// </summary>
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            new TextUI().Execute(args);
+        }
+    }
+}
